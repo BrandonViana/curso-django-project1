7ee9144deea95694e3aeb26c666aa7c0f04f388b
Brandon Viana <brandon.rodrigues.101@gmail.com>
2025-08-21 17:26:37 -0300

    added some changes

---
+++ recipes/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Brandon Rodrigues'
+    })
+
+
+def sobre(request):
+    return HttpResponse('sobre')
+
+
+def contato(request):
+    return HttpResponse('contato')
