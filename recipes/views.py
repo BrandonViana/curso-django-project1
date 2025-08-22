7622028d2b727be0fbb90e05dbef3003168be4e9
Brandon Viana <brandon.rodrigues.101@gmail.com>
2025-08-22 14:52:22 -0300

    removed contato and sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -6,11 +5,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Brandon Rodrigues'
     })
-
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-
-def contato(request):
-    return HttpResponse('contato')
